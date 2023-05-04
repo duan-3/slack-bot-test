c6ea28bf760f1eb95082bc62a540f25ed76b3dc1
Duan <rlawndks30@gmail.com>
2023-05-04 14:27:28 +0900

    #Test: Interaction

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from slack_bolt import App
 from slack_bolt.adapter.fastapi import SlackRequestHandler
 import os
 import ssl
+import random
 ssl._create_default_https_context = ssl._create_unverified_context
 app = FastAPI()
 duan_router = APIRouter()
@@ -25,6 +26,45 @@ def handle_mention(event, say):
     thread_ts = event["thread_ts"]
     thread_ts = event["ts"]
     say("호", thread_ts=thread_ts)
+
+@slack_app.message("?무승뽑")
+def random_seungjae(event, say):
+    blocks = [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*무승뽑(무한 승재 뽑기)*"
+			},
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": "뽑기",
+					"emoji": True
+				},
+				"value": "click_me_123",
+				"action_id": "retry"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "plain_text",
+				"text": ":블루승재::저승재움짤::프링글승재::레드승재::페페승재::아바타승재::타노승재움짤::광대승재::승재::seungjyp::핑크승재::안경승재::짱구승재:",
+				"emoji": True
+			}
+		}
+	]
+    say(blocks=blocks)
+
+# @slack_app.action("retry")
+# def get_reseungjae(ack, say):
+#     ack()
+#     sjs = [":블루승재:", ":저승재움짤:", ":프링글승재:", ":레드승재:", ":페페승재:", ":아바타승재:", ":타노승재움짤:", ":광대승재:", ":승재:", ":seungjyp:", ":핑크승재:", ":안경승재:", ":짱구승재:"]
+#     seungjae = random.choice(sjs)
+#     say(f"{seungjae}")
+
 
 
 app.include_router(duan_router, prefix="/duan")

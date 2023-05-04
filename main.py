0dc5cb795ada02a9edd229ff45dca75c98aec461
Duan <rlawndks30@gmail.com>
2023-05-04 11:21:30 +0900

    #Add: duan_router

--- main.py
+++ main.py
@@ -4,31 +4,27 @@ from slack_bolt import App
 from slack_bolt.adapter.fastapi import SlackRequestHandler
 import os
 import ssl
-import random
 ssl._create_default_https_context = ssl._create_unverified_context
-
 app = FastAPI()
 duan_router = APIRouter()
-
 slack_app = App(
     token=os.environ["DUAN_BOT_TOKEN"],
     signing_secret=os.environ["DUAN_BOT_SIGNING_SECRET"]
 )
-app.include_router(duan_router, prefix="/duan")
-
 @app.get("/")
 async def root():
     return Response
-
-
 # 슬랙 이벤트 수신
 @duan_router.post("/slack/events")
 async def slack_events(request: Request):
     handler = SlackRequestHandler(slack_app)
     return await handler.handle(request)
-
 # 이벤트 처리
 @slack_app.event("app_mention")
 def handle_mention(event, say):
+    thread_ts = event["thread_ts"]
     thread_ts = event["ts"]
     say("호", thread_ts=thread_ts)
+
+
+app.include_router(duan_router, prefix="/duan")

31a223b5b70003273f7200887fcf63a4c37dde0d
Duan <rlawndks30@gmail.com>
2023-05-07 01:59:43 +0900

    #Test: Trigger Channel Id

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ def draw_command(ack, body, client):
             ],
             "private_metadata": trigger_channel_id
         }
-    trigger_id = body["private_metadata"]
+    trigger_id = body["trigger_id"]
     client.views_open(trigger_id=trigger_id, view=view)
 
 
